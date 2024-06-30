26c319b6c090542a0b10e5e6642f3384baa9d8b6
herma <49307226+hermaplusplus@users.noreply.github.com>
2024-06-30 19:10:01 +0100

    Adds helper functions

---
+++ helpers.py
@@ -0,0 +1,31 @@
+import configparser
+import requests
+import json
+
+def set_last_round(config, year, month, day, round):
+    config['meta']['LastRound'] = f"{year}/{month:02}/{day:02}/{round}"
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+def send_webhook(webhook, username=None, avatar_url=None, content=None, file=None, embeds=None):
+    data = {}
+    if username:
+        data["username"] = username
+    if content:
+        data["content"] = content
+    if avatar_url:
+        data["avatar_url"] = avatar_url
+    if embeds:
+        if not isinstance(embeds, list):
+            embeds = [embeds]
+        data["embeds"] = embeds
+
+    headers = {'Content-Type': 'application/json'}
+
+    if file:
+        files = {'file': (file, open(file, 'rb'))}
+        r = requests.post(webhook, files=files, data={'payload_json': json.dumps(data)})
+    else:
+        r = requests.post(webhook, json=data, headers=headers)
+
+    return r
